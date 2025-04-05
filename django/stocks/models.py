5fdecc99e200544887aed5ca776f3ca0bd632fe2
ninthcircle <mainlakshayhoon@gmail.com>
2025-04-05 13:56:06 +0530

    new stocks app, add stocks and uers model, add login-signup functionality

---
+++ django/stocks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Stock(models.Model):
+    stock_id = models.AutoField(primary_key=True)
+    stock_name = models.CharField(max_length=100)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.stock_name
--- django/user/models.py
+++ django/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from stocks.models import Stock
 
-# Create your models here.
+class User(AbstractUser):
+    balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.username
+
+class UserStock(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="stocks")
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ("user", "stock")
